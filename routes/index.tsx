826cb51aed38832e2e6d8bcb21799ca19a99ee86
Josh Guarino <jguarino722@gmail.com>
2022-11-15 15:14:53 -0500

    it beings...

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,15 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+export default function Terminal() {
   return (
-    <>
+    <div class="bg-black h-screen text-white text-lg font-bold">
       <Head>
-        <title>Fresh App</title>
+        <title>JG Terminal</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
+      <main>
+        <div class="text-blue-400">~</div> 
+        <span class="text-green-400">{'>'}</span> <input autoFocus class="bg-black focus:outline-none w-11/12"></input>
+      </main>
+    </div>
   );
 }
