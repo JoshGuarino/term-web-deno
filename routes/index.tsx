a13928b0f7eb33a266e25d38bb8142c19af05ae1
josh-guarino <josh.guarino@eagleview.com>
2022-12-01 17:30:32 -0500

    messing with styles and background

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         <title>Feenix Terminal</title>
       </Head>
       <body 
-        style="background-image: url('city.jpeg');"
-        class="bg-gray-800 h-screen p-2 text-white font-bold flex justify-evenly items-center"
+        style="background-image: url('particles.jpg');"
+        class="bg-gray-800 h-screen p-6 text-white font-bold flex justify-evenly items-center bg-cover"
       >
-        <div class="bg-black opacity-80 border-2 rounded-lg h-4/5 w-4/5 p-2 overflow-auto">
+        <div class="bg-black opacity-80 border-2 rounded-lg h-full w-full p-2 overflow-auto">
           <Terminal />
         </div>
       </body>
