cefa171d4e70927246960fefee8b5757cd8fcfe8
Josh Guarino <jguarino722@gmail.com>
2022-12-04 19:33:20 -0500

    different background etc

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,10 +9,9 @@ export default function Home() {
         <title>JG | Terminal</title>
       </Head>
       <body 
-        style={`background-image: url('${config.backgroundImage}');`}
-        class="bg-gray-800 h-screen p-6 text-white font-bold flex justify-evenly items-center bg-cover"
+        class="bg-gradient-to-b from-gray-800 to-red-800 h-screen p-6 text-white font-bold flex justify-evenly items-center bg-cover"
       >
-        <div class="bg-black opacity-80 border-2 rounded-lg h-full w-full p-2 overflow-auto">
+        <div class="bg-black opacity-70 border-2 rounded-lg h-full w-full p-2 overflow-auto">
           <Terminal user={config.user} host={config.host}/>
         </div>
       </body>
